9107f428eae2229311bbc46a6122889a27d83404
Huysmans Werner <huysmanw@AKDGPORT8474.local>
2018-03-07 00:49:07 +0100

    check

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -25,6 +25,7 @@ export default {
     'page-content': PageContent,
     'aside-menu': Aside
   },
+  middleware: ['check-auth', 'auth'],
   computed: {
     fiche () {
       return this.$store.getters.getInfoFiche(this.$route.params.slug)[0]
